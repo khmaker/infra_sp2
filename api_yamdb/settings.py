49a8b388870f0341c19635ed7a7556c513d49136
khmaker <khmaker@yandex.ru>
2021-02-07 18:47:00 +0300

    to review 9

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -45,59 +45,46 @@ MIDDLEWARE = ['django.middleware.security.SecurityMiddleware',
 ROOT_URLCONF = 'api_yamdb.urls'
 
 TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
-TEMPLATES = [{
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                    'django.template.context_processors.debug',
-                    'django.template.context_processors.request',
-                    'django.contrib.auth.context_processors.auth',
-                    'django.contrib.messages.context_processors.messages', ],
-                    },
-            },
-    ]
+TEMPLATES = [{'BACKEND': 'django.template.backends.django.DjangoTemplates',
+              'DIRS': [TEMPLATES_DIR],
+              'APP_DIRS': True,
+              'OPTIONS': {'context_processors': ['django.template.'
+                                                 'context_processors.debug',
+                                                 'django.template.'
+                                                 'context_processors.request',
+                                                 'django.contrib.auth.'
+                                                 'context_processors.auth',
+                                                 'django.contrib.messages.'
+                                                 'context_processors.'
+                                                 'messages', ], }, }, ]
 
 WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-        'default': {
-                'ENGINE': 'django.db.backends.postgresql',
-                'NAME': os.environ.get('POSTGRES_DB', default='postgres'),
-                'USER': os.environ.get('POSTGRES_USER', default='postgres'),
-                'PASSWORD': os.environ.get('POSTGRES_PASSWORD',
-                                           default='postgres'),
-                'HOST': os.environ.get('DB_HOST'),
-                'PORT': os.environ.get('DB_PORT'),
-                }
-        }
+DATABASES = {'default': {'ENGINE': 'django.db.backends.postgresql',
+                         'NAME': os.environ.get('POSTGRES_DB',
+                                                default='postgres'),
+                         'USER': os.environ.get('POSTGRES_USER',
+                                                default='postgres'),
+                         'PASSWORD': os.environ.get('POSTGRES_PASSWORD',
+                                                    default='postgres'),
+                         'HOST': os.environ.get('DB_HOST'),
+                         'PORT': os.environ.get('DB_PORT'), }}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
 
-AUTH_PASSWORD_VALIDATORS = [
-        {
-                'NAME': 'django.contrib.auth.password_validation'
-                        '.UserAttributeSimilarityValidator',
-                },
-        {
-                'NAME': 'django.contrib.auth.password_validation'
-                        '.MinimumLengthValidator',
-                },
-        {
-                'NAME': 'django.contrib.auth.password_validation'
-                        '.CommonPasswordValidator',
-                },
-        {
-                'NAME': 'django.contrib.auth.password_validation'
-                        '.NumericPasswordValidator',
-                },
-        ]
+AUTH_PASSWORD_VALIDATORS = [{'NAME': 'django.contrib.auth.password_validation'
+                                     '.UserAttributeSimilarityValidator', },
+                            {'NAME': 'django.contrib.auth.password_validation'
+                                     '.MinimumLengthValidator', },
+                            {'NAME': 'django.contrib.auth.password_validation'
+                                     '.CommonPasswordValidator', },
+                            {'NAME': 'django.contrib.auth.password_validation'
+                                     '.NumericPasswordValidator', }, ]
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
@@ -119,19 +106,17 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-REST_FRAMEWORK = {
-        'DEFAULT_PERMISSION_CLASSES': [
-                'rest_framework.permissions.AllowAny',
-                ],
-        'DEFAULT_AUTHENTICATION_CLASSES': [
-                'rest_framework_simplejwt.authentication.JWTAuthentication',
-                ],
-        'DEFAULT_PAGINATION_CLASS': [
-                'rest_framework.pagination.PageNumberPagination'],
-        'PAGE_SIZE': 100,
-        'DEFAULT_FILTER_BACKENDS': [
-                'django_filters.rest_framework.DjangoFilterBackend'],
-        }
+REST_FRAMEWORK = {'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.'
+                                                 'AllowAny', ],
+                  'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework_'
+                                                     'simplejwt.'
+                                                     'authentication.'
+                                                     'JWTAuthentication', ],
+                  'DEFAULT_PAGINATION_CLASS': ['rest_framework.pagination.'
+                                               'PageNumberPagination'],
+                  'PAGE_SIZE': 100,
+                  'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.'
+                                              'DjangoFilterBackend'], }
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
