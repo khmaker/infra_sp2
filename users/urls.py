7f29a31a713773fb00718afbadf4af6320382a47
khmaker <khmaker@yandex.ru>
2021-02-07 18:04:43 +0300

    to review 6

--- users/urls.py
+++ users/urls.py
@@ -1,15 +1,12 @@
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import (
-    UsersViewSet, CreateUserAPIView, GetTokenAPIView
-    )
+
+from .views import (CreateUserAPIView, GetTokenAPIView, UsersViewSet)
 
 v1_router = DefaultRouter()
 v1_router.register('users', UsersViewSet, basename='users')
 
-
-urlpatterns = [
-        path('v1/', include(v1_router.urls)),
-        path('v1/auth/email/', CreateUserAPIView.as_view()),
-        path('v1/auth/token/', GetTokenAPIView.as_view()),
-        ]
+urlpatterns = [path('v1/', include(v1_router.urls)),
+               path('v1/auth/email/', CreateUserAPIView.as_view()),
+               path('v1/auth/token/', GetTokenAPIView.as_view()),
+               ]
