a6f8bc26b1896cbc6e3928c94bf180d40fb07fa6
Maxim Pikul <maxim.pikul@drivepixels.ru>
2021-10-05 18:05:54 +0300

    Optimize model registration

--- src/api/admin.py
+++ src/api/admin.py
@@ -3,8 +3,4 @@ from django.contrib import admin
 
 from api.models import Category, Comment, Genre, Review, Title
 
-admin.site.register(Title)
-admin.site.register(Genre)
-admin.site.register(Category)
-admin.site.register(Comment)
-admin.site.register(Review)
+admin.site.register((Title, Genre, Category, Comment, Review))
